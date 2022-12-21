318cd5ddcc1a529713bff6000e14d8d64e6854dc
matleg <mathieu.leger05@gmail.com>
2022-12-21 15:11:29 +0100

    create 16x16 grid

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-// make one cell
+// make a grid
+
+const gridContainer = document.getElementById("gridContainer");
 
 function makeCell() {
     const cell = document.createElement("div");
     cell.className = "cell";
-    cell.innerHtml = "<p>this is a test</p>"
-    document.getElementById("gridContainer").appendChild(cell);
+    gridContainer.appendChild(cell);
 }
+
+function makeGrid(rows, columns) {
+    for (i = 0; i < columns; i++) {
+        if ( rows <= 1) {
+            for (let j = 0; j < rows; j++) {
+                makeCell();
+            }
+        } else if (rows > 1) {
+            const newColumn = document.createElement("div");
+            gridContainer.appendChild(newColumn);
+            newColumn.className = "newColumn";
+            for (let j = 0; j < rows; j++) {
+                const cell = document.createElement("div");
+                cell.className = "cell";
+                newColumn.appendChild(cell);
+            }
+        }
+    }
+}
+
+makeGrid(16, 16);
